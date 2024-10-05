aef985c94e38651800941454d980096304164403
Chris McHenry <chris@orientedsoftware.com>
2024-10-05 14:54:47 -0600

    Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -16,7 +17,7 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Welcome to GPT Chat",
   description: "Your source for mind-blowing conversations",
-}
+};
 
 export default function RootLayout({
   children,
@@ -25,11 +26,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      `
+      <body className={`${geistSans.className} px-2 md:px-5`}>
+        <header className="text-white font-bold bg-green-900 text-2xl p-2 mb-3 rounded-b-lg shadow-gray-700 shadow-lg flex">
+          <div className="flex flex-grow">
+            <Link href="/">GPT Chat</Link>
+            <Link href="/about" className="ml-5 font-light">
+              About
+            </Link>
+          </div>
+        </header>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-grow">{children}</div>
+        </div>
       </body>
+      `
     </html>
   );
 }
