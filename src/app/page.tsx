047674787bb99b826f2fa910b6041f3a112f3f77
Chris McHenry <chris@orientedsoftware.com>
2024-11-03 08:49:10 -0700

    Interactivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import Chat from "@/app/components/Chat";
+import {Separator} from "@/components/ui/separator";
+import { auth } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
+   
   return (
-    <main className="p-5">
-      <h1 className="text-4xl font-bold">Welcome To GPT Chat</h1>
+    <main>
+      <h1 className="text-4xl font-bold">Welcome to GPT Chat</h1>
+      {!session?.user?.email && <div>You need to log in to use this chat.</div>}
+      {session?.user?.email && (
+        <>
+          <Separator className="my-5" />
+          <Chat />
+        </>
+      )}
     </main>
   );
 }
